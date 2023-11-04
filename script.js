6aef33a981b2686dff74926a881ac84892a88fda
Krishnaparab0508 <100294584+Krishnaparab0508@users.noreply.github.com>
2023-11-04 07:33:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var elem = document.querySelectorAll(".elem")
+elem.forEach(function(val){
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left = dets.x + "px"
+    })
+    val.addEventListener("mouseenter",function(dets){
+        val.childNodes[3].style.opacity = 1
+    })
+    val.addEventListener("mouseleave",function(dets){
+        val.childNodes[3].style.opacity = 0
+    })
+})
